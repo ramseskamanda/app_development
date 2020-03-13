0463149a022947ca2399a879ea64bbb424bec2be
Ramses Kamanda <ramses.kamanda@gmail.com>
2020-03-13 16:30:23 +0100

    update

---
+++ clique/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ramseskamanda/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ramseskamanda/Documents/github.nosync/GitHub/app_development/clique"
+export "FLUTTER_TARGET=/Users/ramseskamanda/Documents/github.nosync/GitHub/app_development/clique/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ramseskamanda/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
